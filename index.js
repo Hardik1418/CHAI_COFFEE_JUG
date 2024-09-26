76259553ec6fc2b8ee0369f55e542c062597ccbf
Hrdk1409 <hrdktech1409@gmail.com>
2024-09-26 15:00:05 +0530

    added path middleware

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const waterJugRouter = require("./routes/addWaterJug.route");
 const billRouter = require("./routes/bill.route");
 const billWaterJugRouter = require("./routes/waterJugBill.route");
 const connectDB = require("./db/dbconfig");
+const path = require("path");
 const app = express();
 const PORT = 2222;
 
 connectDB;
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", homeRouter);
